750a64a5a9915dbd3b58786ef94b4f8cfa284152
alex <57417529+alexanderplotnikov@users.noreply.github.com>
2020-02-22 02:03:31 +0200

    display form with formatted UI

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-const formGet = document.querySelector("#formGet");
+const getForm = document.querySelector("#getForm");
+const form1 = document.getElementById("form_id");
+const submitBtn = document.querySelector("#addBook");
+const cancelBtn = document.querySelector("#cancel");
+const bodyElm = document.querySelector(".container");
+
 
 let myLibrary = [];
 
@@ -11,35 +16,53 @@ function Book() {
 
 }
 
-function addBookToLibrary() {  
+function addBookToLibrary() {
     let userBook = new Book()
     myLibrary.push(userBook)
-} 
-let form1 = document.getElementById("form_id")
-form1.reset();
-console.log(form1)
-let submitBtn = document.querySelector("#addBook");
+}
+
+form1.reset(); // resets form entries on Reload
+
+let key = 0;
 submitBtn.addEventListener("click", () => {
+    
+    
     author = document.querySelector("#author").value;
     title = document.querySelector("#title").value;
     pages = document.querySelector("#pages").value;
-    read = document.querySelector("#read").checked;
-    if(author == '' || title == '' || pages == ''){
+    read = document.querySelector("#read").checked; // returns true or false
+    if (author == '' || title == '' || pages == '') {
         return alert("Missing Information");
     }
-    if(read){
-        read = "Read"
-    }
-    else{
-        read = "Not read"
-    }
+    read ? read = "Read" : read = "Not read";
+    // if (read) {
+    //     read = "Read"
+    // } else { // 0
+    //     read = "Not read"
+    // }
     addBookToLibrary();
     console.log(myLibrary)
     form1.reset();
 
     //Tests
     let box = document.createElement("DIV");
-    box.innerHTML = Object.values(myLibrary[0]);
+    box.innerHTML = Object.values(myLibrary[key]);
     form1.appendChild(box);
+    key++;
 });
 
+getForm.addEventListener("click", () => { 
+    form1.classList.toggle("showForm");
+    bodyElm.classList.toggle("opaque"); 
+    form1.reset();
+});
+cancelBtn.addEventListener("click", () => {
+    form1.classList.toggle("showForm");
+    bodyElm.classList.toggle("opaque");
+    form1.reset();
+});
+bodyElm.addEventListener("click", () => {
+    form1.classList.toggle("showForm");
+    bodyElm.classList.toggle("opaque");
+    form1.reset();
+});
